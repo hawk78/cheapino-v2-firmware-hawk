def03a37e8be1d97ee6ccc4b389143f8e13dfa7b
Vincenzo Di Massa <vincenzo.dimassa@graftholders.com>
2026-09-09 14:05:54 +0200

    baseline: restore upstream cheapino.c verbatim

--- keyboard/cheapino/cheapino.c
+++ keyboard/cheapino/cheapino.c
@@ -64,6 +64,22 @@ void keyboard_post_init_kb(void) {
             ((b & (1 << 14)) >> 13) | \
             b >> 15
 
+/* This is for debugging the matrix rows
+void printBits(uint16_t n)
+{
+    long i;
+    for (i = 15; i >= 0; i--) {
+        if ((n & (1 << i)) != 0) {
+            printf("1");
+        }
+        else {
+            printf("0");
+        }
+    }
+    printf("\n");
+}
+*/
+
 bool bit_pattern_set(uint16_t number, uint16_t bitPattern) {
     return !(~number & bitPattern);
 }
@@ -161,11 +177,15 @@ void fix_encoder_action(matrix_row_t current_matrix[]) {
     static const int ENC_ROW   = 3;
     static const int ENC_A_COL = 2;
     static const int ENC_B_COL = 4;
+    // The button column is unused here and handled through the keymap instead.
+    // static const int ENC_BUTTON_COL = 0;
     static const matrix_row_t ENC_A_BIT = (1 << ENC_A_COL);
     static const matrix_row_t ENC_B_BIT = (1 << ENC_B_COL);
 
+    // State machine tracking.
     static bool colABPressed = false;
 
+    // Check which way the encoder is turned:
     matrix_row_t encoder_row = current_matrix[ENC_ROW];
     bool         colA        = encoder_row & ENC_A_BIT;
     bool         colB        = encoder_row & ENC_B_BIT;
@@ -175,16 +195,20 @@ void fix_encoder_action(matrix_row_t current_matrix[]) {
         colABPressed = true;
     } else if (colA) {
         if (colABPressed) {
+            // A+B followed by A means clockwise
             colABPressed = false;
             encoder_queue_event(0, true);
         }
     } else if (colB) {
         if (colABPressed) {
+            // A+B followed by B means counter-clockwise
             colABPressed = false;
             encoder_queue_event(0, false);
         }
     }
 
+    // Clear A+B bits, and leave the button bits intact; it will be picked up
+    // by normal matrix/keymap processing.
     static const matrix_row_t ROW_MASK = ~(ENC_A_BIT | ENC_B_BIT);
     current_matrix[ENC_ROW]            = encoder_row & ROW_MASK;
 }
